4cdd823d8d1b80a2218fe818e0383e840f4b461e
kit <kit@hvr.world>
2021-08-08 17:04:11 -0400

    correct URL in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='hvr_install',
     version='0.1.0',
     description='A Python utility that installs both public open-source libraries and internal packages',
-    url='git@bitbucket.org:hvrworld/hvr-install.git',
+    url='https://github.com/Hvr-Technologies-Inc/hvr-install.git',
     author='Hvr Technologies Inc',
     author_email='kit@hvr.world',
     packages=find_packages(),
